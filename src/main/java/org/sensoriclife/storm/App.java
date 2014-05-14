f770888bde9eef8ba4abffdec24732eb6bb3219c
jnphilipp <jnphilipp@gmail.com>
2014-05-14 23:07:34 +0200

    Added sensoriclife lib.

--- src/main/java/org/sensoriclife/storm/App.java
+++ src/main/java/org/sensoriclife/storm/App.java
@@ -7,8 +7,8 @@ import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.utils.Utils;
-import java.io.IOException;
-import java.util.Properties;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.sensoriclife.Logger;
@@ -23,12 +23,13 @@ import org.sensoriclife.storm.bolts.WorldBolt;
  * @version 0.0.1
  */
 public class App {
-	private static Properties properties;
-
 	public static void main(String[] args) {
 		Logger.getInstance();
+		org.sensoriclife.Config.getInstance();
 
-		App.loadProperties();
+		Map<String, String> defaults = new LinkedHashMap<>();
+		defaults.put("storm.debug", "false");
+		org.sensoriclife.Config.getInstance().setDefaults(defaults);
 
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setBolt("world", new WorldBolt(), 1).shuffleGrouping("world_generator");
@@ -36,9 +37,9 @@ public class App {
     builder.setBolt("accummolo", new AccumuloBolt(), 10).shuffleGrouping("electricity");
 
 		Config conf = new Config();
-		conf.setDebug(App.getBooleanProperty("storm.debug"));
+		conf.setDebug(org.sensoriclife.Config.getBooleanProperty("storm.debug"));
 
-		if ( !App.getBooleanProperty("storm.debug") ) {
+		if ( !org.sensoriclife.Config.getBooleanProperty("storm.debug") ) {
 			try {
 				Accumulo.getInstance();
 				Accumulo.getInstance().connect("", "", "", "");
@@ -49,7 +50,7 @@ public class App {
 
 			try {
 				conf.setNumWorkers(3);
-				StormSubmitter.submitTopology(App.getProperty("storm.name"), conf, builder.createTopology());
+				StormSubmitter.submitTopology(org.sensoriclife.Config.getProperty("storm.name"), conf, builder.createTopology());
 			}
 			catch ( AlreadyAliveException | InvalidTopologyException e ) {
 				Logger.error("Error while submitting topology.", e.toString());
@@ -69,48 +70,6 @@ public class App {
 			Utils.sleep(10000);
 			cluster.killTopology("test");
 			cluster.shutdown();
-		}
-	}
-
-	/**
-	 * @param key key
-	 * @return values of the given key
-	 */
-	public static String getProperty(String key) {
-		switch (key) {
-		case "storm.debug":
-			return App.properties.getProperty(key, "true");
-		default:
-			return App.properties.getProperty(key, "");
-		}
-	}
-
-	/**
-	 * Returns the value of the given key as boolean;
-	 * @param key key
-	 * @return <code>true</code> or <code>false</code>
-	 */
-	public static boolean getBooleanProperty(String key) {
-		return Boolean.valueOf(App.getProperty(key));
-	}
-
-	/**
-	 * Returns the value of the given key as integer.
-	 * @param key key
-	 * @return integer value
-	 */
-	public static int getIntegerProperty(String key) {
-		return Integer.parseInt(App.getProperty(key));
-	}
-
-	private static void loadProperties() {
-		try {
-			App.properties = new Properties();
-			App.properties.load(App.class.getResourceAsStream("/config.properties"));
-		}
-		catch ( IOException e ) {
-			Logger.error("Error while loading config file.", e.toString());
-			System.exit(1);
 		}
 	}
 }
