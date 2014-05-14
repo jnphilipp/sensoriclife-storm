3cc96c55609fb3d697f3af9e2cf4107b60422fc9
jnphilipp <jnphilipp@gmail.com>
2014-05-14 16:44:31 +0200

    Added test for topology.

--- src/test/java/org/sensoriclife/storm/bolts/WorldBoltTest.java
+++ src/test/java/org/sensoriclife/storm/bolts/WorldBoltTest.java
@@ -3,8 +3,17 @@ package org.sensoriclife.storm.bolts;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Random;
 import org.apache.accumulo.core.client.AccumuloException;
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.TableExistsException;
@@ -17,7 +26,38 @@ import org.sensoriclife.db.Accumulo;
  * @author jnphilipp
  * @version 0.0.1
  */
-public class WorldBoltTest {
+public class WorldBoltTest implements Serializable {
+	private class TestSpout extends BaseRichSpout {
+		private SpoutOutputCollector collector;
+		private Random random;
+
+		public TestSpout() {
+			this.random = new Random(System.currentTimeMillis());
+		}
+
+		@Override
+		public void declareOutputFields(OutputFieldsDeclarer declarer) {
+			declarer.declare(new Fields("user", "billing_address", "other_addresses"));
+		}
+
+		@Override
+		public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+			this.collector = collector;
+		}
+
+		@Override
+		public void nextTuple() {
+			long name = this.random.nextLong();
+			long address = this.random.nextLong();
+			long[] other = new long[this.random.nextInt(5)];
+
+			for ( int i = 0; i < other.length; i++ )
+				other[i] = this.random.nextLong();
+
+			this.collector.emit(new Values(name, address, other));
+		}
+	}
+
 	/**
 	 * Test WorldBolt.
 	 */
@@ -28,8 +68,8 @@ public class WorldBoltTest {
 		try {
 			Accumulo.getInstance();
 			Accumulo.getInstance().connect();
-			Accumulo.getInstance().createTable("users");
-			Accumulo.getInstance().createTable("residentialUnit");
+			Accumulo.getInstance().createTable("users", false);
+			Accumulo.getInstance().createTable("residentialUnit", false);
 		}
 		catch ( AccumuloException | AccumuloSecurityException e ) {
 			Logger.error(WorldBoltTest.class, "Error while connecting to accumulo.", e.toString());
@@ -39,6 +79,7 @@ public class WorldBoltTest {
 		}
 
 		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("world_generator", new TestSpout());
 		builder.setBolt("world", new WorldBolt(), 1).shuffleGrouping("world_generator");
 
 		Config conf = new Config();
